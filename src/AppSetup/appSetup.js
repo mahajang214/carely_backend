f4b9a36e1cd052852ea5d82c159c5b155ea54ca3
mahajang214 <nayanmahajan91@gmail.com>
2026-02-06 23:32:53 +0530

    .

--- src/AppSetup/appSetup.js
+++ src/AppSetup/appSetup.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express=require('express')
 const connectDB = require("../Database/connectDB")
-
+const cors = require('cors')
 // setup express app
 const app=express()
 
@@ -9,12 +9,24 @@ const app=express()
 connectDB();
 
 // cors
+app.use(cors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204   
+}));
 
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 // routes
+// test route
+app.use("/test",(req,res)=>{
+    res.json({message:"API is working fine"})
+})
 
 
 // export app
