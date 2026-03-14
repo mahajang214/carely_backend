83239bc3ab7bf98ef424c6b1f11f92bf2e26f028
mahajang214 <nayanmahajan91@gmail.com>
2026-03-14 12:58:14 +0530

    .

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,14 +1,21 @@
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//   host: process.env.MAIL_HOST,
+//   port: Number(process.env.MAIL_PORT),
+//   secure: false, // true only for port 465
+//   connectionTimeout: 10000,
+//   auth: {
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASS,
+//   },
+// });
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT),
-  secure: false, // true only for port 465
-  connectionTimeout: 10000,
+  service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
-  },
+  }
 });
 
 transporter.verify(function (error, success) {
