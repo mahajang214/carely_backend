321a39ca2929d8d6b637bcc4fdc957324c280911
mahajang214 <nayanmahajan91@gmail.com>
2026-03-14 10:08:11 +0530

    .

--- src/AppSetup/appSetup.js
+++ src/AppSetup/appSetup.js
@@ -17,7 +17,7 @@ connectDB();
 
 // cors
 app.use(cors({
-    origin: true,
+    origin: "https://carely-sage.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
